8619acd515102a23e93163e1de78655aa0986ada
sophieph <so.phouangsy@gmail.com>
2020-07-07 21:34:17 +0200

    test

--- main.py
+++ main.py
@@ -9,5 +9,13 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
+@app.route('/pokemon')
+def pokemon():
+    r = requests.get('https://pokeapi.co/api/v2/pokemon/?limit=100&offset=20')
+
+    list = r.json()
+    return render_template('pokemon.html', list=list)
+    
 if __name__ == "__main__":
     app.run(debug=True)
+
