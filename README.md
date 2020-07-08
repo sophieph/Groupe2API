2d74aff1961a7ce39d6bb59e9fe7c1e1bb07c338
AG RHISSA <a.agrhissa@ecole-ipssi.net>
2020-07-08 16:55:22 +0200

    css

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
+# Pokekoi
+
+API utilisé : 
+* [PokeApi](https://pokeapi.co/) - API sur les Pokemon
+
+### Pré-requis
+
 Installer les paquets suivants :
 
-pip install requests
-pip install flask
-pip install SQLAlchemy
+- pip install requests
+- pip install flask
+- pip install SQLAlchemy
+
+
+## Auteurs
+* **Sophie PHOUANGSY** 
+* **Nassim AKIF**
+* **Alou AG RHISSA**
+
+
--- main.py
+++ main.py
@@ -3,11 +3,56 @@ from flask import render_template
 import requests
 import json
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', 
+            static_folder='web/static',
+            template_folder='web/templates')
 
 @app.route('/')
 def hello():
-    return 'Hello World !'
+    return render_template('index.html')
+
+#Affiche la liste des 151 premiers Pokemon
+@app.route('/pokemon')
+def pokemon():
+    # Liste des 151 premiers pokemon
+    try:
+        r_pokemon = requests.get('https://pokeapi.co/api/v2/pokemon/?limit=151')
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
+
+    list_pokemon = r_pokemon.json()
+    pokemon = list_pokemon['results']
+
+    return render_template('pokemon.html', pokemon=pokemon)
+    
+#Affiche les images de la liste avec la methode AJAX
+@app.route('/pokemon/image/<name>', methods=["POST"])
+def get_image_pokemon(name):
+    url = 'https://pokeapi.co/api/v2/pokemon/' + name
+    try:
+        r_pokemon = requests.get(url)
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
+
+    pokemon = r_pokemon.json()
+    return pokemon
+
+
+
+
+# Affiche la description du pokemon    
+@app.route('/pokemon/<name>')
+def pokemon_by_name(name):
+    url = 'https://pokeapi.co/api/v2/pokemon/' + name
+    try:
+        r_pokemon = requests.get(url)
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
+
+    pokemon = r_pokemon.json()
+    return render_template('pokemon_description.html', pokemon=pokemon, name=name)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
