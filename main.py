1992c7d38158ce9263ba2a49ec40231e0819ef1d
nassimakif <nassim.akif94@gmail.com>
2020-07-09 20:20:20 +0200

    ajout de la description ...!

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from flask import Flask
 from flask import render_template
-import hashlib
-
 import requests
 import json
 
@@ -9,17 +7,9 @@ app = Flask(__name__, static_url_path='',
             static_folder='web/static',
             template_folder='web/templates')
 
-
-
 @app.route('/')
 def mainpage():
     return render_template('index.html')
-
-# Connexion en tant que membre
-@app.route('/login')
-def login():
-    
-    return render_template('login.html')
 
 #Affiche la liste des 151 premiers Pokemon
 @app.route('/pokemon')
@@ -29,14 +19,12 @@ def pokemon():
         r_pokemon = requests.get('https://pokeapi.co/api/v2/pokemon/?limit=151')
     except requests.exceptions.RequestException as e:
         raise SystemExit(e)
-        return page_not_found(e)
 
     list_pokemon = r_pokemon.json()
     pokemon = list_pokemon['results']
 
     return render_template('pokemon.html', pokemon=pokemon)
     
-
 # Affiche la description du pokemon    
 @app.route('/pokemon/<name>')
 def pokemon_by_name(name):
@@ -45,15 +33,20 @@ def pokemon_by_name(name):
         r_pokemon = requests.get(url)
     except requests.exceptions.RequestException as e:
         raise SystemExit(e)
-    
-    status = r_pokemon.status_code
-
-    if r_pokemon.status_code == 404:
-        return render_template('no_pokemon.html'), 404
 
     pokemon = r_pokemon.json()
-    return render_template('pokemon_description.html', pokemon=pokemon, name=name)
 
+    url_description = 'https://pokeapi.co/api/v2/pokemon-species/' + name
+    try:
+        r_pokemon_description = requests.get(url_description)
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
+
+    pokemon = r_pokemon.json()
+    pokemon_description = r_pokemon_description.json()
+    pokemon_description = pokemon_description
+
+    return render_template('pokemon_description.html', pokemon=pokemon, pokemon_description=pokemon_description, name=name)
 
 # Affiche le comparatif entre pokémon
 @app.route('/comparatif')
@@ -74,19 +67,6 @@ def classement_pokemon():
 
     return render_template('classement.html')
 
-# Retourne la recherche   
-@app.route('/pokemon/<name>')
-def search_pokemon(name):
-    url = 'https://pokeapi.co/api/v2/pokemon/' + name
-    try:
-        r_pokemon = requests.get(url)
-    except requests.exceptions.RequestException as e:
-        raise SystemExit(e)
-
-    status = r_pokemon.status_code
-    pokemon = r_pokemon.json()
-    return render_template('pokemon_description.html', pokemon=pokemon, name=name, status=status)    
-
 # Retourne l'API du pokemon avec la methode AJAX
 @app.route('/pokemon/details/<name>', methods=["POST"])
 def get_details_pokemon(name):
@@ -96,23 +76,8 @@ def get_details_pokemon(name):
     except requests.exceptions.RequestException as e:
         raise SystemExit(e)
 
-    if r_pokemon.status_code == 404:
-        list = {}
-        return list
-
     pokemon = r_pokemon.json()
     return pokemon
-
-
-# Page d'erreur pour pokemon non trouve
-app.route('/no_pokemon')
-def pokemon_not_found():
-    return render_template('no_pokemon.html')
-
-# Page d'erreur
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
 
 
 if __name__ == "__main__":
