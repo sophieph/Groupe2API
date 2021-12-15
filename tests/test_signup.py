50c48f63c3c6a75e9831f974866e0da879f7e2fe
textSolver34761 <pradonbenjamin.gmail.com>
2021-12-15 17:54:44 +0100

    tx de couverture 32%

--- tests/test_signup.py
+++ tests/test_signup.py
@@ -12,11 +12,13 @@ def test_index():
 def test_signup():
     print(app)
     tester = app.test_client()
-    response = tester.post('/pokemon/details/undefined', data={"username":"sacha", "email" : "sacha@pokemon.com", "password" : "123456", "password" : "123456"}, follow_redirects=True)
-    print(response.status_code)
-    print()
-    print(response.data)
-    assert 200 == 200
+    response = tester.post('/pokemon/details/undefined',data={
+        "username":"sacha",
+        "email" : "sacha@pokemon.com",
+        "password" : "123456",
+        "password" : "123456"
+    }, follow_redirects=True)
+    assert response.status_code == 200
 
 def test_empty_username_signup():
    tester = app.test_client()
@@ -25,11 +27,26 @@ def test_empty_username_signup():
        "email"    : "sacha@pokemon.com",
        "password" : "123456",
        "password" : "123456"
-       }, follow_redirects=True
-    )
+    }, follow_redirects=True)
    assert response.status_code == 200
 
-# def test_empty_email_signup():
-    # tester = app.test_client()
-    # response = tester.post('/pokemon/details/undefined', data={"username":"sacha", "email" : "", "password" : "123456", "password" : "123456"}, follow_redirects=True)
-    # assert response.status_code == 401*/
+def test_empty_email_signup():
+    tester = app.test_client()
+    response = tester.post('/pokemon/details/undefined', data={
+        "username" :"sacha",
+        "email"    : "",
+        "password" : "123456",
+        "password" : "123456"
+        },
+    follow_redirects=True)
+    assert response.status_code == 200
+
+def test_empty_password_signup():
+   tester = app.test_client()
+   response = tester.post('/pokemon/details/undefined', data={
+       "username" :"sacha",
+       "email"    : "sacha@pokemon.com",
+       "password" : "",
+       "password" : ""
+       }, follow_redirects=True)
+   assert response.status_code == 200
